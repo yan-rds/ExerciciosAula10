2802f7bcc7986dbf76cbf49956452d3e03d5a24c
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-08-30 13:31:05 -0300

    Exercicio 1

---
+++ src/br/com/zup/Exercicio1.java
@@ -0,0 +1,15 @@
+package br.com.zup;
+
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+	// 1 - Faça um programa que mostra na tela os números de 1 a 100.
+        int contador = 1;
+
+        while (contador <= 100) {
+            System.out.println(contador);
+            contador ++;
+
+        }
+    }
+}
